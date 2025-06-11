338d1817f844e58ea2cace4018d418b5a6cda239
Lema-TR <teferalema03@gmail.com>
2025-06-12 00:33:22 +0300

    file rename

--- app/src/main/java/com/example/food_ordering_system/Activity/CartActivity.kt
+++ app/src/main/java/com/example/food_ordering_system/Activity/CartActivity.kt
@@ -11,7 +11,7 @@ class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_to_cart)
+        setContentView(R.layout.activity_cart)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
